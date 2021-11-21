9f1d97ac02b8292ff87038ef6290921903b22fa9
VineetEkka <56101893+VineetEkka@users.noreply.github.com>
2021-11-20 21:42:33 -0500

    Update SocialPrototypeProj.R

--- SocialPrototypeProj.R
+++ SocialPrototypeProj.R
@@ -5,6 +5,8 @@ library(lubridate)
 library(geniusr)
 library(dplyr)
 library(tidytext)
+library(genius)
+library(geniusr)
 
 
 Spotifycreds <- read_json("SpotifyCreds.json") 
@@ -19,7 +21,22 @@ Spotifyaccess_token <- get_spotify_access_token()
 
 Geniuscreds <- read_json("GeniusCreds.json") 
 
-genius_token(force = TRUE)
+#genius Creds
+genius_token <- function(force = FALSE)
+{
+  env <- Sys.setenv('kYKnu6jb5qu0ZEEcuA1jsvKVSbw45fgAH00ct-HcIVbziF3Hl9slWHmyESTiN1Eo') #replace with your own Genius token
+  if (!identical(env, "") && !force) return(env)
+  
+  if (!interactive()) {
+    stop("Please set env var GENIUS_API_TOKEN to your Genius API key",
+         call. = FALSE)
+  }
+  
+  message("Couldn't find env var GENIUS_API_TOKEN See ?genius_token for more details.")
+  message("Please enter your Genius Client Access Token and press enter:")
+  pat <- readline(": ")
+  
+  if (identical(pat, "")) {
 
 
 get_song(song_id = 90479)
