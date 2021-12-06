62ffbde7faa30805a2ca6fb167a39ffc028d1b6d
VineetEkka <56101893+VineetEkka@users.noreply.github.com>
2021-12-06 17:40:33 -0500

    Update SocialPrototypeProj.R

--- SocialPrototypeProj.R
+++ SocialPrototypeProj.R
@@ -1,54 +1,33 @@
-library(spotifyr)
 library(tidyverse)
 library(jsonlite)
 library(lubridate)
 library(geniusr)
 library(dplyr)
 library(tidytext)
-library(genius)
-library(geniusr)
+library(RedditExtractoR)
+library(SentimentalAnalysis)
+library(redditr)
+library(devtools)
 
 
-Spotifycreds <- read_json("SpotifyCreds.json") 
+#write.csv(EdmSet,"C:/Users/Vineet Ekka/Desktop/Edmset.csv")
 
-Sys.setenv(SPOTIFY_CLIENT_ID = Spotifycreds$id)
-Sys.setenv(SPOTIFY_CLIENT_SECRET = Spotifycreds$secret)
+#RedditExtractoR::get_thread_content()
 
-Spotifyaccess_token <- get_spotify_access_token()
-
+#RedditExtractoR::find_thread_urls("EDM")
 
 
 
-Geniuscreds <- read_json("GeniusCreds.json") 
+edm <- find_thread_urls(sort_by = "top",subreddit = "Electronic Music",period = "week")
+urls <-edm$url[[5]]
+edmCom <- get_thread_content(urls)
 
-#genius Creds
-genius_token <- function(force = FALSE)
-{
-  env <- Sys.setenv('kYKnu6jb5qu0ZEEcuA1jsvKVSbw45fgAH00ct-HcIVbziF3Hl9slWHmyESTiN1Eo') #replace with your own Genius token
-  if (!identical(env, "") && !force) return(env)
-  
-  if (!interactive()) {
-    stop("Please set env var GENIUS_API_TOKEN to your Genius API key",
-         call. = FALSE)
-  }
-  
-  message("Couldn't find env var GENIUS_API_TOKEN See ?genius_token for more details.")
-  message("Please enter your Genius Client Access Token and press enter:")
-  pat <- readline(": ")
-  
-  if (identical(pat, "")) {
-    stop("Genius API key entry failed", call. = FALSE)
-  }
-  
-  message("Updating GENIUS_API_TOKEN env var to PAT")
-  Sys.setenv(GENIUS_API_TOKEN = pat)
-  
-  pat
-}
-genius_lyrics(artist = "Margaret Glaspy", song = "Memory Street")
+com<-edm$comments
+comments <- com$comment
 
 
-get_song(song_id = 90479)
+
+
 
 
 
