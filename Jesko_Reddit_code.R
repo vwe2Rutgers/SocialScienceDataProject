0a1b0572a6728bce6fc9554d7eefd61c2218a564
JeskoG <90991053+JeskoG@users.noreply.github.com>
2021-12-10 02:18:43 -0500

    Updated code

--- Jesko_Reddit_code.R
+++ Jesko_Reddit_code.R
@@ -1,8 +1,17 @@
 
+<<<<<<< Updated upstream
 library(jsonlite)
 library(httr)
 library(RedditExtractoR)
 library(tidyverse)
+=======
+library(tidyverse)
+library(jsonlite)
+library(lubridate)
+library(dplyr)
+library(RedditExtractoR)
+library(SentimentalAnalysis)
+>>>>>>> Stashed changes
 library(devtools)
 library(pushshiftR)
 library(knitr)
@@ -18,6 +27,7 @@ library(magrittr)
 
 #Gathering Reddit comments to build text corpora
 
+<<<<<<< Updated upstream
 rap <- getPushshiftData(postType = "comment",
                  size = 1000,
                  after = "1637841600",
@@ -70,3 +80,42 @@ words.2 %>% count(word, sort = TRUE) %>%
 
 top.terms <- words.2 %>% group_by(subreddit) %>% count(word, sort = TRUE) %>% top_n(10, n)
 
+=======
+edm <- find_thread_urls(sort_by = "top",subreddit = "ElectronicMusic",period = "year")
+#filtering out inactive posts
+edmMax <-edm %>% filter(comments > 10)
+#checkin number of comments
+edmsum <- edm %>% group_by(date_utc) %>% summarize(e.sum = sum(comments))
+#Getting all the URLs
+urlo <-edmMax$url
+#Gathering thread contents to get comments and metadata
+thread.contents <- lapply(urlo, get_thread_content)
+#looping over thread.contents to filter out comments
+EDM_comments <- tibble()
+for (n in 1:length(thread.contents)) {
+  EDM_comments <- rbind(comments, edm.thread.contents[[n]][[2]])
+}
+
+write.csv(EDM_comments, "C:\\Users\\jesko\\Documents\\GitHub\\SocialScienceDataProject\\EDM_comments.csv")
+
+# data <- data %>% 
+#   mutate(body = gsub("#[A-Za-z0-9]+|@[A-Za-z0-9]", "", body)) %>% # Removing hashtags and mentions
+#   mutate(body = gsub("(http[^ ]*)|(www.[^ ]*)", "", body)) %>% # Removing URLs
+#   distinct(body, .keep_all =TRUE)
+# 
+# words <- data %>% unnest_tokens(word, body)
+# 
+# 
+# data(stop_words) 
+# stop_words <- stop_words %>% filter(lexicon == "snowball") # specifying snowball stopword lexicon
+# words.2 <- words %>% anti_join(stop_words)
+# 
+# words.2 %>% count(word, sort = TRUE) %>%
+#   slice(1:10) %>%
+#   mutate(word = reorder(word, n)) %>%
+#   ggplot(aes(n, word)) + geom_col() +
+#   labs(y = NULL, x='Term frequency', title=paste("10 most frequent terms in corpus"))
+# 
+# top.terms <- words.2 %>% group_by(subreddit) %>% count(word, sort = TRUE) %>% top_n(10, n)
+# 
+>>>>>>> Stashed changes
