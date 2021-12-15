57135288347387170f4f1d1be88bec676d5eb5f4
VineetEkka <56101893+VineetEkka@users.noreply.github.com>
2021-12-14 21:39:31 -0500

    Create app.R

---
+++ app.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(shinydashboard)
+
+
+ui <- dashboardPage(
+  
+  
+  
+  
+  
+  
+)
+
+
+server <- function(input,output, session){
+  
+  
+  
+  
+}
+
+
+#ui <- fluidPage()
+
+
+shinyApp(ui, server)
+
+C:/Users/HP/OneDrive/Desktop/Cs111/SocialScienceDataProject/SocialPrototypeProj.R
